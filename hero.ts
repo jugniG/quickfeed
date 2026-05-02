aacca11a1dcd80e6f51c12e4f6baf353f17fe5d8
Sahil Gupta <guptas3067@gmail.com>
2026-05-02 20:57:00 +0530

    init

---
+++ hero.ts
@@ -0,0 +1,5 @@
+// hero.ts
+import { heroui } from "@heroui/react";
+export default heroui({
+  themes: { light: { colors: { secondary: "#4F39F6", primary: 'black' } }, dark: { colors: { secondary: '#4F39F6' } } },
+});
