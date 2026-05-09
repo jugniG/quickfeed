aa4a55737e76c5a67a8e4edb876f9a0fcaac4beb
jugniG <jugni@quickfeed.dev>
2026-05-09 18:50:43 +0000

    Remove .vercel from repo, add to .gitignore

--- .vercel/output/functions/__server.func/_protected-B_a4b6PG.mjs
+++
@@ -1,9 +0,0 @@
-import { n as require_jsx_runtime } from "./_libs/@heroui/kbd+[...].mjs";
-import { o as Outlet } from "./_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/_protected-B_a4b6PG.js
-var import_jsx_runtime = require_jsx_runtime();
-function ProtectedLayout() {
-	return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(Outlet, {});
-}
-//#endregion
-export { ProtectedLayout as component };
--- .vercel/output/functions/__server.func/_ssr/kysely-adapter-CNyDyfmW.mjs
+++
@@ -1,2 +0,0 @@
-import { i as createKyselyAdapter, o as kyselyAdapter } from "../_libs/@better-auth/kysely-adapter+[...].mjs";
-export { createKyselyAdapter, kyselyAdapter };
